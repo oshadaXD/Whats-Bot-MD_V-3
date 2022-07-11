a1819c48f6d4dd5c0e2f1b5d96a9669dc5c3a3d5
AiDarkEzio <scienceposhitha@gmail.com>
2022-07-11 10:36:02 +0530

    Ai_Dark_Ezio is (GroupParticipantsUpdate component created) ✔

--- components/GroupParticipantsUpdate.js
+++ components/GroupParticipantsUpdate.js
@@ -1,47 +1,60 @@
-const groupParticipantsUpdate = (event, conn) =>{
-   try {
-            let metadata = await conn.groupMetadata(anu.id)
-            let participants = anu.participants
-            for (let num of participants) {
-                // Get Profile Picture User
-                try {
-                    ppuser = await conn.profilePictureUrl(num, 'image')
-                } catch {
-                    ppuser = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
-                }
+const { getBuffer } = require('../lib/myfunc')
+const { D_E_TMB } = require('../lib/config')
 
-                //Get Profile Picture Group\\
-                try {
-                    ppgroup = await conn.profilePictureUrl(anu.id, 'image')
-                } catch {
-                    ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
-                }
+const GroupParticipantsUpdate = async (event, conn) =>{
+    try {
+        let metadata = await conn.groupMetadata(event.id);
+        let members = event.participants;
+        for (let member of members) {
 
-                //welcome\\
-                let nama = await conn.getName(num)
-                memb = metadata.participants.length
+            try { // Get Profile Picture User
+                ppUser = await conn.profilePictureUrl(member, 'image')
+            } catch {
+                ppUser = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+            };
+            try { //Get Profile Picture Group
+                ppGroup = await conn.profilePictureUrl(event.id, 'image')
+            } catch {
+                ppGroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+            };
+            let welcome = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURIComponent(ppUser)}&name=${encodeURIComponent(memberName)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`);
+            let goodbye = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppUser)}&name=${encodeURIComponent(memberName)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`);
+            const actions = ['add', 'remove', 'promote', 'demote'];
+            let memberName = await conn.getName(member);
+            let memb = metadata.participants.length;
 
-                Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
+            if (event.action == actions[0]) {
 
-                Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
-                if (anu.action == 'add') {
-                    conn.sendMessage(anu.id, {
-                        image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ Hi👋 @${num.split("@")[0]},
+                conn.sendMessage(event.id, { image: welcome, contextInfo: { mentionedJid: [member] }, caption: `
+⭐✑ Hi👋 @${member.split("@")[0]},
 ⭐✑ Welcome To ${metadata.subject}
 
-⭐✑ Description: ${metadata.desc}
+⭐✑ Description:\n ${metadata.desc}
 
-⭐✑ Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`})
-                } else if (anu.action == 'remove') {
-                    conn.sendMessage(anu.id, {
-                        image: Tol, contextInfo: { mentionedJid: [num] }, caption: `⭐✑ @${num.split("@")[0]} Left ${metadata.subject}
+⭐✑ Welcome To Our Comfortable Happy😋!`});
 
-⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` })
-                }
+            } else if (event.action == actions[1]) {
+
+                conn.sendMessage(event.id, { image: goodbye, contextInfo: { mentionedJid: [member] }, caption: `
+⭐✑ @${member.split("@")[0]} Left ${metadata.subject}
+
+⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` });
+
+            } else if (event.action = actions[2]) {
+
+                conn.sendMessage(event.id, { text: `⭐✑ Hi👋 @${member.split("@")[0]}\n\n *You Are New Group admin*`, contextInfo: { mentionedJid: [member], "externalAdReply": { "title": `Dark_Ezio`, "body": ` New Group Admin`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": fs.readFileSync(`../Media/gojo.jpg`), "sourceUrl": D_E_TMB } } })
+                
+            } else if (event.action = actions[3]) {
+                
+                conn.sendMessage(event.id, { text: `⭐✑ Hi👋 @${member.split("@")[0]}\n\n *You Are Not Group admin*`, contextInfo: { mentionedJid: [member], "externalAdReply": { "title": `Dark_Ezio`, "body": ` New Group Admin`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": fs.readFileSync(`../Media/gojo.jpg`), "sourceUrl": D_E_TMB } } })
+
             }
-        } catch (err) {
-            print(err)
-        }
-     
+        };
+    } catch (err) {
+        console.log(err);
+    };  
+};
+
+module.exports = {
+    GroupParticipantsUpdate
 }
--- index.js
+++ index.js
@@ -49,7 +49,8 @@ const path = require('path')
 const PhoneNumber = require('awesome-phonenumber')
 const mongoDB = require('./lib/mongoDB')
 const pino = require('pino');
-const { WhatsBotStart } = require('./lib/defunc')
+const { WhatsBotStart } = require('./lib/defunc');
+const { GroupParticipantsUpdate } = require('./components/GroupParticipantsUpdate');
 const print = console.log
 const port = 8030
 var low
@@ -138,51 +139,8 @@ async function DarkEzio_Whats_Bot() {
         await GroupUpdate(groupMetaData, conn);
     });
 
-    conn.ev.on('group-participants.update', async (anu) => {
-        try {
-            let metadata = await conn.groupMetadata(anu.id)
-            let participants = anu.participants
-            for (let num of participants) {
-                // Get Profile Picture User
-                try {
-                    ppuser = await conn.profilePictureUrl(num, 'image')
-                } catch {
-                    ppuser = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
-                }
-
-                //Get Profile Picture Group\\
-                try {
-                    ppgroup = await conn.profilePictureUrl(anu.id, 'image')
-                } catch {
-                    ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
-                }
-
-                //welcome\\
-                let nama = await conn.getName(num)
-                memb = metadata.participants.length
-
-                Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
-
-                Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
-                if (anu.action == 'add') {
-                    conn.sendMessage(anu.id, {
-                        image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ Hi👋 @${num.split("@")[0]},
-⭐✑ Welcome To ${metadata.subject}
-
-⭐✑ Description: ${metadata.desc}
-
-⭐✑ Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`})
-                } else if (anu.action == 'remove') {
-                    conn.sendMessage(anu.id, {
-                        image: Tol, contextInfo: { mentionedJid: [num] }, caption: `⭐✑ @${num.split("@")[0]} Left ${metadata.subject}
-
-⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` })
-                }
-            }
-        } catch (err) {
-            print(err)
-        }
+    conn.ev.on('group-participants.update', async (event) => {
+        GroupParticipantsUpdate(event, conn)
     })
 
     //Setting\\
